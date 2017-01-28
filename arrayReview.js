0f67c11d39a83176f3876da84338b1ce98e118a9
Richard Wurth <richard.wurth91@gmail.com>
2017-01-28 13:44:51 -0700

    Initial and final submission of arrayReview

--- arrayReview.js
+++ arrayReview.js
@@ -4,16 +4,36 @@ var threeItems = [1,2,3];
 
   //code here
 
+function last (array) {
+  let result = [];
+  for (var i = 0; i < array.length; i++) {
+    result = array[i];
+  }
+  return result;
+}
 
+alert(last(threeItems));
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Loop through evenArray removing all values that aren't even 
+//Loop through evenArray removing all values that aren't even
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
   //code here
+
+function allEvens (array) {
+  let result = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
+
+evenArray = allEvens(evenArray);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -28,6 +48,17 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
   //code here
 
+function finder (random,array) {
+  let result = false;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === random) {
+      result = true;
+    }
+  }
+  return result;
+}
+
+alert(finder(getRandomArbitrary(),randomArray));
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -38,6 +69,8 @@ var first = [1,2,3,4,5];
 var second;
 
   //code here
+
+second = first.slice().concat([first.length+1,first.length+2]);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
@@ -53,22 +86,61 @@ var sentence = "Dev Mountain is the best"
 
   //code here
 
+  function longest (string) {
+    let array = string.split(" ");
+    let result = [];
+    for (var i = 0; i < array.length; i++) {
+      if (array[i].length > result.length) {
+        result = array[i];
+      }
+    }
+    return result;
+  }
+
+  longest(sentence);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//write a function called capitalize that takes in the myPoem variable and capitalizes every word 
+//write a function called capitalize that takes in the myPoem variable and capitalizes every word
 var myPoem = 'What is a jQuery but a misunderstood object?'
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
   //code here
 
+  function capitalize (string) {
+    let result = "";
+    for (var i = 0; i < string.length; i++) {
+      if (!string[i-1] || string[i-1] === " ") {
+        result += string[i].toUpperCase();
+      } else {
+        result += string[i];
+      }
+    }
+    return result;
+  }
 
+myPoem = capitalize(myPoem);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
+
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+
+
+function vowelCounter (string) {
+  let upperCase = string.toUpperCase();
+  let result = 0;
+  for (var i = 0; i < upperCase.length; i++) {
+    if(upperCase[i] == "A" || upperCase[i] == "E" || upperCase[i] == "I" || upperCase[i] == "O" || upperCase[i] == "U" || upperCase[i] == "Y") {
+      result += 1;
+    }
+  }
+  return result;
+}
+
+console.log(vowelCounter(theOdyssey));
